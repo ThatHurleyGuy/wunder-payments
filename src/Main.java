7624acc47dfc33f79a258b85915a2dfad33e890f
Sean Hurley <sean.hurley6@gmail.com>
2021-08-06 12:20:08 -0600

    Remove oops

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        System.out.println("asdfasdf");
         URI uri = new URI("https://gist.githubusercontent.com/ThatHurleyGuy/53148d2d83b820ac1d5d2b78e9322a90/raw/9e882f9fefe44e6c6c4f80897f1f2d16cbbc5e1b/loan-payments.json");
         JSONTokener tokener = new JSONTokener(uri.toURL().openStream());
         JSONArray root = new JSONArray(tokener);
